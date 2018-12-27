93549efff220605b663f8445d7476d9a1a9ccf54
Oleg Parkhomenko <Oleg.Parkhom@gmail.com>
2018-12-28 01:48:56 +0200

    При отправке формы используется функция upload и отменено действие формы по умолчанию. Окно загрузки изображения закрывается, как только данные успешно сохраняются

--- js/backend.js
+++ js/backend.js
@@ -4,6 +4,14 @@
   var URL = 'https://js.dump.academy/kekstagram';
 
   window.backend = function (data, onLoad) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
 
+    xhr.addEventListener('load', function () {
+      onLoad(xhr.response);
+    });
+
+    xhr.open('POST', URL);
+    xhr.send(data);
   };
 })();
