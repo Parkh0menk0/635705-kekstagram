d12f16a8392d8963b6169a81e128ab97c8396df7
Oleg Parkhomenko <Oleg.Parkhom@gmail.com>
2019-01-04 00:34:25 +0200

    Добавил обработчики изменения фильтров, которые будут управлять порядком отрисовки элементов на странице: Популярные, Новые, Обсуждаемые

--- js/gallery.js
+++ js/gallery.js
@@ -2,9 +2,10 @@
 
 (function () {
   var similarListPictures = document.querySelector('.pictures');
-  var similarPictureTemplate = document.querySelector('#picture')
-    .content
-    .querySelector('.picture');
+  var similarPictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+  var filters = document.querySelector('.img-filters');
+  var filterType = document.querySelectorAll('.img-filters__button');
 
   var getPictureElement = function (description) {
     var pictureElement = similarPictureTemplate.cloneNode(true);
@@ -35,7 +36,54 @@
   };
 
   window.backend.load(renderPictures, onError);
-})();
 
-var imgFilters = document.querySelector('.img-filters');
-imgFilters.classList.remove('img-filters--inactive');
+  var picturesArr = []; // инициализация для линта
+  // var picturesArr = renderPictures( ?? );
+
+  filters.classList.remove('img-filters--inactive');
+
+  filterType[0].addEventListener('click', function () {
+    var picture = similarListPictures.querySelectorAll('.picture');
+    for (var i = 0; i < picture.length; i++) {
+      similarListPictures.removeChild(picture[i]);
+    }
+    renderPictures(picturesArr);
+  });
+
+  var unique = function (arr) {
+    var obj = {};
+    for (var i = 0; i < arr.length; i++) {
+      var str = arr[i];
+      obj[str] = true; // запомнить строку в виде свойства объекта
+    }
+    var uniqueArray = Object.keys();
+    uniqueArray.length = 10;
+    return uniqueArray;
+  };
+
+  filterType[1].addEventListener('click', function () {
+    var picture = similarListPictures.querySelectorAll('.picture');
+    for (var i = 0; i < picture.length; i++) {
+      similarListPictures.removeChild(picture[i]);
+    }
+    renderPictures(unique(picturesArr));
+  });
+
+  filterType[2].addEventListener('click', function () {
+    var picture = similarListPictures.querySelectorAll('.picture');
+    for (var i = 0; i < picture.length; i++) {
+      similarListPictures.removeChild(picture[i]);
+    }
+    var picturesCopyArr = picturesArr.slice().sort(function (first, second) {
+      var result = 0;
+      if (first.comments.localeCompare(second.comments) === 1) {
+        result = 1;
+      }
+      if (first.comments.localeCompare(second.comments) === -1) {
+        result = -1;
+      }
+      return result;
+    });
+    renderPictures(picturesCopyArr);
+  });
+})();
